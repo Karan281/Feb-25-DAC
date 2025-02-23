e09f585331945b5ab174246fedae48bc1a1f3470
Karan281 <125893704+Karan281@users.noreply.github.com>
2025-02-23 16:41:57 +0530

    Add files via upload

---
+++ assignment 1/Area.java
@@ -0,0 +1,8 @@
+class Area{
+	public static void main(String[] args) {
+	int radius = 7;
+	double area = 3.14159 * radius * radius;
+	// instand of radius * radius we can use math.pow(base ,exponent)
+	System.out.println("area of circle = " + area);
+	}
+}
---
+++ assignment 1/Multiplyingprogram.java
@@ -0,0 +1,10 @@
+class Multiplyingprogram{
+	public static void main(String[] args){
+	
+   
+        int FirstNumber = 50;
+        int SecondNumber = 5;
+	
+	System.out.println("First and Second number multiply is = " + (FirstNumber * SecondNumber));
+	}
+}
